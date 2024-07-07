49a61fdf28b185a0b842e3097f58cd0bdc58af1f
Henrique Botella <42356846+databotella@users.noreply.github.com>
2024-07-07 12:30:06 -0300

    Refatora a estrutura da imagem de logo na tela de login
    - Substitui a tag <img> por uma <div> com background-image
    - Aplica animações de entrada na logo e no container de login
    - Ajusta a responsividade da logo e do container
    - Implementa solução com jQuery para manipulação de background-image
    - Corrige a ordem de inclusão dos scripts no HTML

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const path = require('path');
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 360,
+        height: 780,
         webPreferences: {
             preload: path.join(__dirname, 'frontend/public/preload.js'), // Atualize o caminho para o preload.js
-            nodeIntegration: true,
-            contextIsolation: false,
+            nodeIntegration: false, // Desativa a integração do Node.js
+            contextIsolation: true, // Mantém o isolamento do contexto
+            enableRemoteModule: false
         },
+        resizable: false,
+        maximizable: false,
+        fullscreenable: false
     });
 
     mainWindow.loadFile(path.join(__dirname, 'frontend/public/index.html')); // Atualize o caminho para o index.html
